0b809bc1588ab84c01803e0c36f286a9430d4142
Hartsa <harrinupponen@gmail.com>
2019-12-10 15:25:48 +0200

    another commit

--- src/main/java/postgre/test/demo1/domain/PgTest.java
+++ src/main/java/postgre/test/demo1/domain/PgTest.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class PgTest {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String text;
 	
